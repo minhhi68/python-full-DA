8bbcf2854adba818ea6372c551598610755c188d
minhhi68 <80388522+minhhi68@users.noreply.github.com>
2025-05-19 14:47:11 +0700

    first commit

---
+++ bai2/day2.py
@@ -0,0 +1,9 @@
+#VD1: Điều kiện if
+dtb = float(input("Your average GPA:  "))
+if dtb >= 5: 
+    print("You passed")
+    print("Congrats");
+    
+x = 250000.24
+print('giam gia: ', '{:,.4f}'.format(x))
+
---
+++ bai_1/day1.py
@@ -0,0 +1,17 @@
+chuoi = 'Toi hoc Python'
+print(chuoi)
+
+chuoi1 = "toi hoc 'Python'"
+print(chuoi1)
+
+chuoi2 = "Day la \"Minh Huy\""
+print(chuoi2)
+
+chuoi3 = "Toi la 'Minh Huy' , \"Henry\""
+print(chuoi3)
+
+diem1 = 9.8
+diem2 = 9.5
+
+dtb = (diem1 + diem2) /2
+print('Trung binh: ' , dtb)
